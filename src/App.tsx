df13835d1cccc33894f7414abe05f461054a86fd
Ronny Haase <ronnyhaase@fastmail.com>
2025-02-13 09:26:55 +0100

    Add basic UI template

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,59 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
+	fetch(
+		"https://www.alphavantage.co/query?function=OVERVIEW&symbol=IBM&apikey=demo",
+	);
 	return (
 		<>
-			<h1>Hello, world!</h1>
+			<h1>Stock Terminal</h1>
+			<table width="100%">
+				<tbody>
+					<tr>
+						<td colSpan={2}>
+							Symbol <input />
+							<button>Search</button>
+						</td>
+					</tr>
+					<tr>
+						<td width="50%">"Chart"</td>
+						<td width="50%">
+							<h2>Company Data</h2>
+							<table border={1} width="100%">
+								<tbody>
+									<tr>
+										<td>Name</td>
+										<td>The Symbol, Co.</td>
+									</tr>
+									<tr>
+										<td>Description</td>
+										<td>
+											The Symbol, Co. is the ultimate
+											symbol for nothing!
+										</td>
+									</tr>
+									<tr>
+										<td>Symbol</td>
+										<td>SMBL</td>
+									</tr>
+									<tr>
+										<td>Marlet Cap</td>
+										<td>0</td>
+									</tr>
+									<tr>
+										<td
+											colSpan={2}
+											style={{ textAlign: "center" }}
+										>
+											...
+										</td>
+									</tr>
+								</tbody>
+							</table>
+						</td>
+					</tr>
+				</tbody>
+			</table>
 		</>
 	);
 }
